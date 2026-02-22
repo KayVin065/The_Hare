f3e3b53d2ef0dfa1761651c8020f6e4dd288cedc
Kayla Vincent <kaylavincent165@outlook.com>
2026-02-22 15:46:44 -0600

    Started work on prototype. Working on scripting for player.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float moveForce = 10f;
+
+    [SerializeField]
+    private float jumpForce = 10f;
+
+    private float movementX;
+    private RigidBody2D myBody;
+    private Animator anim;
+    private SpriteRenderer sr;
+
+    private string GROUND_TAG = "Ground";
+    private bool isGrounded = true;
+
+    private void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMoveKeyboard();
+    }
+
+    void PlayerMoveKeyboard()
+    {
+        movementX = Input.GetAxisRaw("Horizontal");
+        transform.position += new Vector3(movementX, 0f, 0f) * Time.deltaTime * moveForce;
+    }
+
+
+}
