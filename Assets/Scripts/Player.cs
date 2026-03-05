7bc47427a8a276f609a76a324f5ac40d7537a0be
Kayla Vincent <kaylavincent165@outlook.com>
2026-03-05 13:18:06 -0600

    Fixed buggy player controls!!! Still need to add dig and eat functionality.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,22 +3,40 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    public float moveSpeed = 10f;
-    public float sprintSpeed = 5f;
+    // Movement
+    public float moveSpeed = 8f;
+    public float sprintMultiplier = 1.5f;
+    private float currentSpeed;
+
+    // Jump
     public float jumpForce = 10f;
     public float fallMultiplier = 2.5f;
+
+    // Jump Assist
+    public float coyoteTime = 0.1f;
+    public float jumpBufferTime = 0.1f;
+    private float coyoteTimeCounter;
+    private float jumpBufferCounter;
+
+    // Ground Check
+    public Transform groundCheck;
+    public float groundRadius = 0.2f;
+    public LayerMask groundLayer;
 
     private PlayerInput playerInput;
     private Rigidbody2D rb;
     private Animator anim;
     private SpriteRenderer sr;
+
     private Vector2 moveInput;
 
-    private string GROUND_TAG = "Ground";
+    //private string GROUND_TAG = "Ground";
     private string FOOD_TAG = "Plant";
     //private string DIG_TAG = "Dig";
 
-    private bool isGrounded = true;
+    private bool isGrounded;
+
+    // Abilities
     private bool isEdible = false;
     //private bool isDiggable = true;
 
@@ -29,21 +47,19 @@ public class Player : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         sr = GetComponent<SpriteRenderer>();
+
+        currentSpeed = moveSpeed;
     }
 
     private void OnEnable()
     {
         playerInput.Player.Enable();
 
-        playerInput.Player.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
-        playerInput.Player.Move.canceled += ctx => moveInput = Vector2.zero;
+        playerInput.Player.Jump.performed += OnJump;
+        playerInput.Player.Sprint.started += OnSprintStart;
+        playerInput.Player.Sprint.canceled += OnSprintStop;
 
-        playerInput.Player.Jump.performed += ctx => TryJump();
-
-        playerInput.Player.Sprint.started += ctx => moveSpeed += sprintSpeed;
-        playerInput.Player.Sprint.canceled += ctx => moveSpeed -= sprintSpeed;
-
-        playerInput.Player.Eat.performed += ctx => TryEat();
+        //playerInput.Player.Eat.performed += ctx => TryEat();
     }
 
     private void OnDisable()
@@ -58,24 +74,63 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        Vector3 move = new Vector3(moveInput.x, 0f, moveInput.y) * moveSpeed * Time.deltaTime;
-        transform.Translate(move);
+        moveInput = playerInput.Player.Move.ReadValue<Vector2>();
 
-        if(rb.linearVelocityY < 0)
-            rb.linearVelocityY += Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, groundLayer);
+
+        if(isGrounded) 
+            coyoteTimeCounter = coyoteTime;
+        else
+            coyoteTimeCounter -= Time.deltaTime;
+
+        jumpBufferCounter -= Time.deltaTime;
+
+        if (jumpBufferCounter > 0 && coyoteTimeCounter > 0) {
+            Jump();
+            jumpBufferCounter = 0;
+        }
+
+        FlipSprite();
     }
 
-    private void TryJump()
+    private void FixedUpdate() 
     {
-        if (!isGrounded) return;
+        rb.linearVelocity = new Vector2(moveInput.x * currentSpeed, rb.linearVelocityY);
 
-        Jump();
+        if (rb.linearVelocity.y < 0)
+        {
+            rb.linearVelocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.fixedDeltaTime;
+        }
     }
+
+    void OnJump(InputAction.CallbackContext ctx) 
+    {
+        jumpBufferCounter = jumpBufferTime;
+    }
+
     private void Jump()
     {
         Debug.Log("Jump pressed");
-        isGrounded = false;
+        coyoteTimeCounter = 0;
         rb.linearVelocity = new Vector2(rb.linearVelocityX, jumpForce);
+    }
+
+    void OnSprintStart(InputAction.CallbackContext ctx)
+    {
+        currentSpeed = moveSpeed * sprintMultiplier;
+    }
+
+    void OnSprintStop(InputAction.CallbackContext ctx)
+    {
+        currentSpeed = moveSpeed;
+    }
+
+    void FlipSprite()
+    {
+        if (moveInput.x > 0)
+            sr.flipX = false;
+        else if (moveInput.x < 0)
+            sr.flipX = true;
     }
 
     private void TryEat()
@@ -95,16 +150,10 @@ public class Player : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.CompareTag(GROUND_TAG)) {
-            isGrounded = true;
-            Debug.Log("Ground touched");
-        }
-
         if(collision.gameObject.CompareTag(FOOD_TAG))
         {
             isEdible = true;
         }
 
     }
-
 }
