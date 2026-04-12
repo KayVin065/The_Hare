2a4576ee3dc714cd4daad638f8c1171656a98db8
Kayla Vincent <kaylavincent165@outlook.com>
2026-04-12 18:07:40 -0500

    Complete level tiling for tutorial. Update camera script

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -3,18 +3,22 @@ using UnityEngine;
 public class CameraScript : MonoBehaviour
 {
     private Transform player;
-    private Vector3 tempPos;
+    private Vector3 tempPos = Vector3.zero;
+    private Rigidbody2D playerRB;
+    
+    public float moveSpeed;
+    public float lookAheadDist = 1f, lookAheadSpeed = 3f;
+    private float lookOffset;
 
-    [SerializeField] private float minX, maxX;
-    [SerializeField] private float minY, maxY;
-
-    [SerializeField] private float stepHeight = 6f;
-    [SerializeField] private float yOffset = 0f;
-    [SerializeField] private float vertLerpSpeed = 8f;
+    [SerializeField] private float minX, maxX, minY, maxY;
+    [SerializeField] private float offset;
+    [SerializeField] private float velThreshold = 0.1f;
 
     void Start()
     {
         player = GameObject.FindWithTag("Player").transform;
+        playerRB = player.GetComponent<Rigidbody2D>();
+        tempPos = new Vector3(player.position.x, player.position.y, transform.position.z);
     }
 
     void LateUpdate()
@@ -22,19 +26,26 @@ public class CameraScript : MonoBehaviour
         if(!player)
             return;
 
-        tempPos = transform.position;
-        tempPos.x = player.position.x;
+        //tempPos = transform.position;
+
+        //tempPos.x = player.position.x;
+        tempPos.y = player.position.y + offset;
+
+        float rbx = playerRB.linearVelocity.x;
+
+        if(Mathf.Abs(rbx) > velThreshold)
+        {
+            if(rbx > 0f)
+                lookOffset = Mathf.Lerp(lookOffset, lookAheadDist, lookAheadSpeed * Time.deltaTime);
+            else
+                lookOffset = Mathf.Lerp(lookOffset, -lookAheadDist, lookAheadSpeed * Time.deltaTime);
+        }
+
+        tempPos.x = player.position.x + lookOffset;
 
         tempPos.x = Mathf.Clamp(tempPos.x, minX, maxX);
+        tempPos.y = Mathf.Clamp(tempPos.y, minY, maxY);
 
-        // Calculating camera "steps"
-        float zone = Mathf.Floor(player.position.y / stepHeight);
-
-        float targetY = zone * stepHeight + yOffset;
-
-        float clampedTargetY = Mathf.Clamp(targetY, minY, maxY);
-        tempPos.y = Mathf.Lerp(transform.position.y, clampedTargetY, vertLerpSpeed * Time.deltaTime);
-
-        transform.position = tempPos;
+        transform.position = Vector3.Lerp(transform.position, tempPos, moveSpeed * Time.deltaTime);
     }
 }
