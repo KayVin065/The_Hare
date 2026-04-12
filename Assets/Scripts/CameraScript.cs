57335d13364447b2617960f2a5886f945efce2f8
Kayla Vincent <kaylavincent165@outlook.com>
2026-04-11 20:45:35 -0500

    Begin tilemap painting for tutorial. Update camera for vertical changes

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -5,8 +5,12 @@ public class CameraScript : MonoBehaviour
     private Transform player;
     private Vector3 tempPos;
 
-    [SerializeField]
-    private float minX, maxX;
+    [SerializeField] private float minX, maxX;
+    [SerializeField] private float minY, maxY;
+
+    [SerializeField] private float stepHeight = 6f;
+    [SerializeField] private float yOffset = 0f;
+    [SerializeField] private float vertLerpSpeed = 8f;
 
     void Start()
     {
@@ -21,15 +25,16 @@ public class CameraScript : MonoBehaviour
         tempPos = transform.position;
         tempPos.x = player.position.x;
 
-        if(tempPos.x < minX) {
-            tempPos.x = minX;
-        }
+        tempPos.x = Mathf.Clamp(tempPos.x, minX, maxX);
 
-        if(tempPos.x > maxX) {
-            tempPos.x = maxX;
-        }
+        // Calculating camera "steps"
+        float zone = Mathf.Floor(player.position.y / stepHeight);
+
+        float targetY = zone * stepHeight + yOffset;
+
+        float clampedTargetY = Mathf.Clamp(targetY, minY, maxY);
+        tempPos.y = Mathf.Lerp(transform.position.y, clampedTargetY, vertLerpSpeed * Time.deltaTime);
 
         transform.position = tempPos;
-        
     }
 }
